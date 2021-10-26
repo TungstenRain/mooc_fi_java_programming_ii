83d73dd070022ed0e2c1c2af51a2f72193b37ad2
Frank Olson <frank.olson@gmail.com>
2021-10-26 17:05:10 -0500

    first commit

---
+++ part11-Part11_04.StudentAndUniversity/src/main/java/Student.java
@@ -0,0 +1,9 @@
+/**
+ * Student Class for Part 11_04
+ * @author frank
+ */
+public class Student {
+    private int studentID;
+    private String name;
+    private University university;
+}
---
+++ part11-Part11_04.StudentAndUniversity/src/main/java/University.java
@@ -0,0 +1,12 @@
+/**
+ * University Class for Part11_04
+ * @author frank
+ */
+
+import java.util.ArrayList;
+
+public class University {
+    // Variables
+    private String name;
+    private ArrayList<Student> students;
+}
