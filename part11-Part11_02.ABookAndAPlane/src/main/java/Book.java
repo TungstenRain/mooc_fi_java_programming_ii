d836bf0e1f8e4a3eeab22a0ea247f753929d039e
Frank Olson <frank.olson@gmail.com>
2021-10-26 17:04:50 -0500

    first commit

---
+++ part11-Part11_02.ABookAndAPlane/src/main/java/Book.java
@@ -0,0 +1,10 @@
+/**
+ * Book Class for Part11_02
+ * @author frank
+ */
+public class Book {
+    // Variables
+    private String name;
+    private String author;
+    private int pageCount;
+}
---
+++ part11-Part11_02.ABookAndAPlane/src/main/java/Plane.java
@@ -0,0 +1,10 @@
+/**
+ * Plane Class for Part11_02
+ * @author frank
+ */
+public class Plane {
+    // Variables
+    private String ID;
+    private String model;
+    private int yearOfIntroduction;
+}
