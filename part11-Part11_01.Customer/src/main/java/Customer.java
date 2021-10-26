08464c9797e3b2ceff87e7a7b539a0c0be1a6c9b
Frank Olson <frank.olson@gmail.com>
2021-10-26 17:04:42 -0500

    first commit

---
+++ part11-Part11_01.Customer/src/main/java/Customer.java
@@ -0,0 +1,10 @@
+/**
+ * Customer Class for Part11_01
+ * @author frank
+ */
+public class Customer {
+    // Variables
+    private String name;
+    private String address;
+    private String email;
+}
