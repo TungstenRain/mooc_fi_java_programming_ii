51efcbd4ff19fabbe5443cf62952326f28679cf0
Frank Olson <frank.olson@gmail.com>
2021-10-29 10:57:16 -0500

    first commit

---
+++ part11-Part11_05.ThePlayerAndTheBot/src/main/java/Bot.java
@@ -0,0 +1,14 @@
+/**
+ * Bot Class for Part11_05
+ * @author frank
+ */
+public class Bot extends Player {
+    // Methods
+    @Override
+    public void play() {
+        
+    }
+    public void addMove() {
+        
+    }
+}
---
+++ part11-Part11_05.ThePlayerAndTheBot/src/main/java/Player.java
@@ -0,0 +1,16 @@
+/**
+ * Player Class for Part11_05
+ * @author frank
+ */
+public class Player {
+    // Variables
+    private String name;
+    
+    // Methods
+    public void play() {
+        
+    }
+    public void printName() {
+        
+    }
+}
