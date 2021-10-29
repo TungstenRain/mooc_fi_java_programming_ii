db42cc02449918ef06bf8342e9c5e4df845f669b
Frank Olson <frank.olson@gmail.com>
2021-10-29 10:57:38 -0500

    first commit

---
+++ part11-Part11_06.SaveablePerson/src/main/java/Person.java
@@ -0,0 +1,26 @@
+/**
+ * Person Class for Part11_06
+ * @author frank
+ */
+public class Person implements Saveable {
+    // Variables
+    private String name;
+    private String address;
+    
+    // Methods
+    @Override
+    public void save() {
+        
+    }
+
+    @Override
+    public void delete() {
+        
+    }
+
+    @Override
+    public void load(String address) {
+        
+    }
+    
+}
---
+++ part11-Part11_06.SaveablePerson/src/main/java/Saveable.java
@@ -0,0 +1,11 @@
+/**
+ * Saveable Interface for Part 11_06
+ * @author frank
+ */
+public interface Saveable {
+    // Methods
+    public void save();
+    public void delete();
+    public void load(String address);
+    
+}
